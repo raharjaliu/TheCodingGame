c32e10f4064cbbe2aea8bfbd36f7ac8d51339f1d
Emilka <emilia.lozinska@tum.de>
2016-12-11 02:00:43 +0100

    Changed the package name

---
+++ guessing_game/scripts/guessing_game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from roboy import Roboy
+from sys import stdin
+
+class GuessingGame:
+    def __init__(self):
+        import os
+        import shlex
+
+        assert 'command' in os.environ
+        self.roboy = Roboy(shlex.split(os.environ['command']))
+        self.roboy.start()
+
+    def ask(self, sentence):
+        print('Asking: {0}'.format(sentence))
+        self.roboy.write(sentence)
+
+    def read(self):
+        return "\n".join([action['speak'] for action in self.roboy.read()])
+
+    def run(self):
+        print(self.read())
+        for line in stdin:
+            line = line.strip("\n")
+            print(line)
+            self.ask(line)
+            print(self.read())
+        
+
+## @brief Initialization function of the node.
+#
+if __name__ == "__main__":
+    rospy.init_node('guessing_game')
+
+    rospy.loginfo('going to start roboy guessing game')
+
+    game = GuessingGame()
+    game.run()
+
+    rospy.spin()
